1bdd3f44d336d18785aedd90f3102a26fb1784d4
isalin <isa@teste.com>
2024-11-04 17:26:34 -0300

    Construindo a estrutura e a seção "Sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/isallin.png" />
+    <Titulo fontSize={20}>Isabel Lin</Titulo>
+  </aside>
+)
+
+export default Sidebar
