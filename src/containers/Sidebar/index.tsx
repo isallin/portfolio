a7797d4910019ee13c2ee14b5cb0e9b5b49d02f9
isalin <isa@teste.com>
2024-11-08 17:06:18 -0300

    estilizando sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/isallin.png" />
-    <Titulo fontSize={20}>Isabel Lin</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Isabel Lin</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Isallin
+      </Paragrafo>
+      <Descricao fontSize={12}>Engenheiro front-end</Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
